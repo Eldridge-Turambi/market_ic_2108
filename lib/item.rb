da4daa7e1ffcf42118603a6e53fc7e4530fee639
Eldridge Turambi <eturambi95@gmail.com>
2021-09-28 14:09:43 -0600

    Git practice with making new branches

--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,1 @@
-class Item
-  attr_reader :name,
-              :price
 
-  def initialize(item_info)
-    @name = item_info[:name]
-    @price = item_info[:price].gsub('$', '').to_f #delete would work too instead of gsub
-  end
-end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,17 +1,1 @@
-require './lib/item'
 
-RSpec.describe Item do
-  before :each do
-    @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-  end
-
-  it 'is an instance of Item' do
-    expect(@item1).to be_an_instance_of(Item)
-  end
-
-  it 'has a name and price attributes' do
-    expect(@item2.name).to eq("Tomato")
-    expect(@item1.price).to eq(0.75)
-  end
-end
